56a30758e5e0bb925bfef68038014e2a351373f2
Richard Lee <lifuzu@gmail.com>
2014-09-03 23:25:30 -0700

    start jenkins with runit

--- images/jenkins/build.sh
+++ images/jenkins/build.sh
@@ -1,1 +1,1 @@
-sudo docker build -rm -t weimed/docker-jenkins:1.1 .
+sudo docker build -rm -t weimed/docker-jenkins:1.2 .
--- images/jenkins/jenkins.sh
+++ images/jenkins/jenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-exec java -jar /usr/share/jenkins/jenkins.war --prefix=$JENKINS_PREFIX
+#JAVA_OPTS=$JAVA_OPTS -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses
+JENKINS_PREFIX=/
+exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war --prefix=$JENKINS_PREFIX
